18b85f645d0f4924378d42f693214c41873041bd
Rocco <sadarocco@gmail.com>
2021-03-20 17:44:20 -0300

    update annotations

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # typescript
+
+Use of types give us an easy way to refer the different properties and functions that a value has
+
+## primitive types
+- number
+- boolean
+- void
+- undefined
+- string
+- symbol
+- null
+
+## object types
+- functions
+- arrays
+- classes
+- objects
---
+++ features/annotations/variables.ts
@@ -0,0 +1,30 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// array of strings
+let colors: string[] = ['red', 'green', 'blue'];
+
+// array of numbers
+let myNumbers: number[] = [1, 2, 3];
+
+// arary of booleans
+let truths: boolean[] = [true, false, true]
+
+// Classes
+class Car {
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number; } = {
+  x: 10,
+  y: 20
+}
